6aadf4c69d91d7ee4c9d94546dda12672ac48b06
[Home]yoon pyo <ypyo09@naver.com>
2017-10-31 01:35:38 +0900

    login Page

--- src/main/java/kr/or/dgit/SaleManagement/MainApp.java
+++ src/main/java/kr/or/dgit/SaleManagement/MainApp.java
@@ -1,13 +1,42 @@
 package kr.or.dgit.SaleManagement;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-
+	private Stage primaryStage;
+	private Parent rootLayout;
+	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("영업관리 프로그램");
 		
+//		fontLoad();
+		
+		initRootLayout();
+	}
+
+	private void fontLoad() {
+//		Font.loadFont(getClass().getResourceAsStream("view/font/NotoSansCJKkr-Black.otf"),14);		
+	}
+
+	private void initRootLayout() throws IOException  {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(MainApp.class.getResource("view/loginView.fxml"));
+		rootLayout = (AnchorPane) loader.load();
+		
+		Scene scene = new Scene(rootLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
