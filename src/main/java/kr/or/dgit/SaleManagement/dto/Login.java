707d71060dde42842afcda1e49cf18ebcf14f164
GurnMi <dlrjsak3@naver.com>
2017-11-10 15:45:47 +0900

    ui 수정

--- src/main/java/kr/or/dgit/SaleManagement/dto/Login.java
+++ src/main/java/kr/or/dgit/SaleManagement/dto/Login.java
@@ -5,10 +5,12 @@ public class Login {
 	private String pw;
 	private boolean admit;
 	private int code;
+
 	public Login() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Login(String id, String pw, boolean admit, int code) {
 		super();
 		this.id = id;
@@ -16,35 +18,42 @@ public class Login {
 		this.admit = admit;
 		this.code = code;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPw() {
 		return pw;
 	}
+
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
+
 	public boolean isAdmit() {
 		return admit;
 	}
+
 	public void setAdmit(boolean admit) {
 		this.admit = admit;
 	}
+
 	public int getCode() {
 		return code;
 	}
+
 	public void setCode(int code) {
 		this.code = code;
 	}
+
 	@Override
 	public String toString() {
 		return String.format("%s %s %s %s", id, pw, admit, code);
 	}
-	
-	
-	
+
 }
